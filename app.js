10cf3f74de91753ec2549f7fccee29f6303d793d
Davy <davy@user.lan>
2024-06-20 15:03:02 +0100

    modified content

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
 });
 
 
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 // app.get("/", (req, res) => {res.send })
